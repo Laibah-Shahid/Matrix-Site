6723ac06cf365b18ad0e13d5c07e983a3ceec3d2
Laibah-Shahid <laibahshahid18@gmail.com>
2024-04-23 23:09:27 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import MatrixRainingCode from './components/MatrixRainingEffect';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Features from './components/Section1';
-import Testimonials from './components/Section2';
-import Pricing from './components/Section3';
+import Features from './components/Features';
+import Testimonials from './components/Testimonials';
+import Pricing from './components/Pricing';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-hidden">
       <MatrixRainingCode className="absolute inset-0" />
       <div className="flex flex-col">
         <Navbar />
